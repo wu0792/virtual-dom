4f2acc90992574f608b868453fcdfae8b6ee2951
wumm <wumm@Ctrip.com>
2019-06-24 16:38:27 +0800

    modify the element.js

--- build/bundle.js
+++ build/bundle.js
@@ -90,21 +90,21 @@ app.appendChild(root.render());
 
 "use strict";
 class Element {
-    constructor(type, props = {}, children = []) {
-        this.type = type;
+    constructor(tagName, props = {}, children = []) {
+        this.tagName = tagName;
         this.props = props;
         this.children = Array.isArray(children) ? children : [children];
     }
 
     render() {
-        if (typeof this.type === 'string') {
-            const element = document.createElement(this.type);
+        if (typeof this.tagName === 'string') {
+            const element = document.createElement(this.tagName);
             Object.keys(this.props).forEach(key => element.setAttribute(key, this.props[key]));
             this.children.forEach(child => element.appendChild(typeof child === 'string' ? document.createTextNode(child) : child.render()));
 
             return element;
         } else {
-            throw 'only support type of string';
+            throw 'only support tagName of string';
         }
     }
 }
